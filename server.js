83bad0968020d9ee8aa731153f70fdc61bef5d0a
ASHOK-MOHANTA <subendum25@gmail.com>
2025-09-23 11:57:20 +0530

    Add normal response

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
+app.get("/",(req,res)=>{
+  res.send({activeStatus:true,error:false})
+})
 
 const server = http.createServer(app);
 
